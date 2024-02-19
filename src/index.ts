c116355728f5b542491c48b34042e72cedb2ae2e
MishaKozarev <mishaaaaa1986@mail.ru>
2024-02-19 18:01:46 +0300

    feat: add typescript dependencie in project

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { httpServer } from "./src/http_server/index.js";
+import { httpServer } from "./http_server/index";
 
-const HTTP_PORT = 8181;
+const HTTP_PORT: number = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
